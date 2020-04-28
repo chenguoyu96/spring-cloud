d82cc3594a5312686990d7e1effe00218ca6144f
chenguoyu <chenguoyu96@foxmail.com>
2020-04-28 22:02:55 +0800

    集成spring-gateway

---
+++ cloud-gateway-gateway9527/src/main/java/org/chenguoyu/springcloud/filter/ServerGatewayFilter.java
@@ -0,0 +1,23 @@
+package org.chenguoyu.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class ServerGatewayFilter implements GatewayFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("ServerGatewayFilter filter ");
+        return chain.filter( exchange );
+    }
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
